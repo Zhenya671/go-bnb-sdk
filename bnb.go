d0415584e7c26220435b9a04d96403448d9ac57c
zhenya <yauheni.turban@innowise-group.com>
2022-08-19 18:10:20 +0300

    realised get response func

--- bnb.go
+++ bnb.go
@@ -1,59 +1,64 @@
 package go_bnb_sdk
 
 import (
+	"encoding/json"
 	"errors"
+	"fmt"
+	"io"
+	"log"
 	"net/http"
-	"time"
+	"strconv"
 )
 
 const (
-	host           = "https://www.nbrb.by/api/exrates/currencies"
-	defaultTimeout = 5 * time.Second
+	apiURL = "https://www.nbrb.by/api/exrates/currencies/"
 )
 
-type (
-	addRequest struct {
-		URL          string `json:"url"`
-		CurrencyName string
-	}
+var ErrFailedAPICall = errors.New("bad response from BookBeta API")
 
-	AddInput struct {
-		URL string
-	}
-)
-
-func (i AddInput) validate() error {
-	if i.URL == "" {
-		return errors.New("required URL value is empty")
-	}
-	return nil
+type Currency struct {
+	CurID           int    `json:"Cur_ID"`
+	CurParentID     int    `json:"Cur_ParentID"`
+	CurCode         string `json:"Cur_Code"`
+	CurAbbreviation string `json:"Cur_Abbreviation"`
+	CurName         string `json:"Cur_Name"`
+	CurNameBel      string `json:"Cur_Name_Bel"`
+	CurNameEng      string `json:"Cur_Name_Eng"`
+	CurQuotName     string `json:"Cur_QuotName"`
+	CurQuotNameBel  string `json:"Cur_QuotName_Bel"`
+	CurQuotNameEng  string `json:"Cur_QuotName_Eng"`
+	CurNameMulti    string `json:"Cur_NameMulti"`
+	CurNameBelMulti string `json:"Cur_Name_BelMulti"`
+	CurNameEngMulti string `json:"Cur_Name_EngMulti"`
+	CurScale        int    `json:"Cur_Scale"`
+	CurPeriodicity  int    `json:"Cur_Periodicity"`
+	CurDateStart    string `json:"Cur_DateStart"`
+	CurDateEnd      string `json:"Cur_DateEnd"`
 }
 
-func (i AddInput) generateRequest(currencyName string) addRequest {
-	return addRequest{
-		URL:          i.URL,
-		CurrencyName: currencyName,
-	}
-}
-
-type Client struct {
-	client       *http.Client
-	currencyName string
-}
-
-func NewRequest(currencyName string) (*Client, error) {
-	if currencyName == "" {
-		return nil, errors.New("currency name doesn't specified")
+func GetCurrentCurrency(currentCurrencyID int) (map[string]interface{}, error) {
+	id := strconv.Itoa(currentCurrencyID)
+	res, err := http.Get(apiURL + id)
+	if err != nil {
+		log.Fatal(ErrFailedAPICall)
 	}
 
-	return &Client{
-		client: &http.Client{
-			Timeout: defaultTimeout,
-		},
-		currencyName: currencyName,
-	}, nil
-}
+	defer func(Body io.ReadCloser) {
+		err := Body.Close()
+		if err != nil {
+			log.Fatal("connect with api cannot be closed")
+		}
+	}(res.Body)
 
-func (c *Client) doHTTP() {
+	if res.StatusCode != http.StatusOK {
+		return nil, fmt.Errorf("unexpected status code: %d - %s: %w", res.StatusCode, res.Status, ErrFailedAPICall)
+	}
 
+	var gotCurrency map[string]interface{}
+	err = json.NewDecoder(res.Body).Decode(&gotCurrency)
+	if err != nil {
+		return nil, fmt.Errorf("could not decode json: %s\n", err)
+	}
+
+	return gotCurrency, nil
 }
